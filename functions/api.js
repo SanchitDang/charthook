044842ca04fc10148a3d01500d18fba66e30cdab
Sanchit <work.sanchitdang@gmail.com>
2024-12-12 21:10:20 +0530

    netlify

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const serverles = require('serverless-http');
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON data
@@ -45,7 +46,8 @@ app.get('/events', (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
+module.exports.handler = serverless(app)
